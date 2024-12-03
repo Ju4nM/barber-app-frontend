1c32249026c885daf6792feb0047d4c020cc4daa
Ju4nM <jenriquemflores@gmail.com>
2024-12-03 10:29:06 -0700

    2da organizacion de imports

--- src/modules/Routes/routes.tsx
+++ src/modules/Routes/routes.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import StylePage from "../StylePage/stylePage";
 import PrincipalPage from "../PrincipalPage/PrincipalPage";
